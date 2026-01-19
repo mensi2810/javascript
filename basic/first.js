f45f153f9faa74a45cbf2e92403284fdecb713a6
mensi2810 <mensilapani28@gmail.com>
2026-01-19 20:37:33 +0530

    basic

---
+++ basic/first.js
@@ -0,0 +1,26 @@
+// console.log("mensi");
+
+// var ame="mensi";
+// console.log(ame);
+
+// let fullname="mensi";
+// let age=24;
+// let price=99.99;
+// let radius=14;
+// x=null;
+// y=undefined;
+// isFollow=true;
+// const a=28
+// console.log(fullname);
+// console.log(age);
+// console.log(a);
+
+const student={
+    fullname : "mensi",
+    age :20,
+    cgpa :8.2,
+    ispass:true
+}
+console.log(student.age);
+console.log(student["cgpa"]);
+student["fullname"] = "mensi";
---
+++ basic/practice.js
@@ -0,0 +1,10 @@
+const product = {
+    title: "pen",
+    rating: 4,
+    offer: 5,
+};
+const profile = {
+    username: "@mensilapani",
+    isFollow: false,
+    follower: 123,
+};
