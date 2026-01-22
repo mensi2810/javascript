8c46603dbd3be2041aeb9bdfaa59ec12b33968b7
mensi2810 <mensilapani28@gmail.com>
2026-01-22 20:18:43 +0530

    loop

---
+++ loops/index.js
@@ -0,0 +1,17 @@
+//-------for loop -------------
+
+/*
+for (let i=1; i<=5; i++){
+
+    console.log("mensi");//5 time execute
+}*/
+
+//-----calculate sum of 1 to 5----------
+
+let sum = 0;
+
+for (let i=0; i<=5;i++){
+    sum= sum +i;
+}
+console.log("sum =",sum);
+
