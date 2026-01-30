368ca42db02cc25076f1a87a3090f2ddee98bb62
mensi2810 <mensilapani28@gmail.com>
2026-01-30 18:23:47 +0530

    DOM

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,67 @@
 console.log("hello");
+
+//console.dir(document.body);
+//console.log(document.head);
+
+let heading=document.getElementById("heading");//id na base pr
+console.dir(heading);
+
+
+
+let button=document.getElementById("myid");
+console.dir(button);
+
+//-----class name access-----------
+
+let headings=document.getElementsByClassName("heading");
+
+console.dir(headings);
+console.log(headings);
+
+//-------tag access --------------
+
+let parah= document.getElementsByTagName("p");
+console.dir(parah);
+
+
+
+//------------query selector--------
+
+let ele = document.querySelector("p");
+console.dir(ele);    //1 element return tag mate
+
+
+let all = document.querySelectorAll("p");
+console.dir(all);// all element 
+
+
+let elem = document.querySelector(".heading");
+console.dir(elem);    //1 element return class mate
+
+
+//--all id mate query selector not allowed
+
+let buttn = document.querySelector("#myid");
+console.dir(buttn);    //1 element return
+
+//---------property---
+
+//1.tagName:
+buttn.tagName //output:BUTTON
+
+//2.innerText:
+
+let div = document.querySelector("div");
+console.dir(div);//div.innerText
+
+
+//3.div.innerHTML
+
+//4.textContent
+
+//div.innerText="hello world";
+
+//heading.textContent
+
+
+
---
+++ DOM/practice.js
@@ -0,0 +1,96 @@
+//----practice task 1-----------
+
+let h2= document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText= h2.innerText + " from Apna college Student";//concate string
+
+
+//----practice task 2-----------
+
+let divs= document.querySelectorAll(".box");
+console.log(divs);
+let idx=1;
+for (div of divs)
+{
+   div.innerText=`new unique value ${idx}`;
+   idx++;
+}
+// divs[0].innerText="new unique value 1";
+// divs[1].innerText="new unique value 2";
+
+
+//-----------------Attributes-----------
+//1.getattributes
+
+let mydiv=document.querySelector("div");
+console.log(mydiv);
+
+
+let val=div.getAttribute("id");
+console.log(val);
+
+let parah =document.querySelector("p");
+console.log(parah.getAttribute("class"));
+
+//2.set attributes
+
+let para =document.querySelector("p");
+console.log(para.setAttribute("class","new class")); 
+
+let div2 = document.querySelector("div");
+
+div2.style.backgroundColor ="green";
+div2.style.fontSize="20px";
+
+
+//------insert element----
+
+let newbtn = document.createElement("button");
+console.log(newbtn);
+
+newbtn.innerText = "click me";
+
+//1.node.append method
+//add at the end of node
+
+let div1= document.querySelector("div");
+//div.append(newbtn);
+
+//2.  node.prepend method
+//add at the start of node(inside)
+
+//div.prepend(newbtn);
+
+//3. node.before method
+//add before the node (outside)
+
+div.before(newbtn);
+
+//4. node.after method
+//add after the node (outside)
+
+//para.after(newbtn);
+
+
+//----------delete element---------
+
+//1. node.remove method
+//remove the node itself
+
+let par1= document.querySelector("p");
+//par1.remove();
+
+//------------practice task 3-------------
+
+let button = document.createElement("button");
+button.innerText="Click me";
+
+button.style.backgroundColor="red";
+button.style.color="white";
+
+document.querySelector("body").prepend(button);
+
+
+//------------practice task 4-------------
+
